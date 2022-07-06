14104b608941a4728ff209a1a5cff56dda84ab8c
innokenty <eazy-mf-e@mail.ru>
2022-07-06 23:53:43 +0300

    hellousername

---
+++ hellousername/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа");
+{
+    Consoel.writeLine("Ура, это же НАТАША!");
+}
+else
+{
+    Consoel.Write("Привет ,");
+    Console.WeiteLine(username);
+}
